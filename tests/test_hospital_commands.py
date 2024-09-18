11d45d78b074308060a14bb64575aefc6cdf5562
Дмитриев Сергей Сергеевич <se.dmitriev@rt-solar.ru>
2024-09-18 14:16:31 +0300

    9

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -10,7 +10,6 @@ class TestHospitalCommands:
         hospital = Hospital(patients=[0,2])
         console = MagicMock()
         hospital_commands = HospitalCommands(hospital, console)
-
         console.input.return_value = 1
 
         hospital_commands.get_patient_status()
@@ -18,3 +17,13 @@ class TestHospitalCommands:
         console.input.assert_called_once_with('Введите ID пациента: ')
         console.print.assert_called_once_with('Статус пациента: "Тяжело болен"')
 
+    def test_up_patient_status(self):
+        hospital = Hospital(patients=[2, 0])
+        console = MagicMock()
+        hospital_commands = HospitalCommands(hospital, console)
+        console.input.return_value = 1
+
+        hospital_commands.up_patient_status()
+
+        console.input.assert_called_once_with('Введите ID пациента: ')
+        console.print.assert_called_once_with('Новый статус пациента: "Готов к выписке"')
