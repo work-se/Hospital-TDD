f2884fbf9e3f29eb6f6f26e316ef91abff5b9a63
Дмитриев Сергей Сергеевич <se.dmitriev@rt-solar.ru>
2024-09-18 14:18:00 +0300

    10

--- hospital_commands.py
+++ hospital_commands.py
@@ -14,3 +14,12 @@ class HospitalCommands:
         status = self._hospital.get_status(patient_id)
 
         self._console.print(f'Статус пациента: "{status}"')
+
+    def up_patient_status(self):
+        patient_id_as_text = self._console.input("Введите ID пациента: ")
+        patient_id = int(patient_id_as_text)
+
+        self._hospital.status_up(patient_id)
+        status = self._hospital.get_status(patient_id)
+
+        self._console.print(f'Новый статус пациента: "{status}"')
